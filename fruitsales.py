178965a00295ad70a5eb47197c30934464242a69
Matt Gibson <gibson.matthew@hotmail.com>
2023-12-17 20:28:21 -0500

    Add Dataframe to csv code

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,10 @@
 # add your code here
+import pandas as pd
+
+df = pd.DataFrame({'Apples': ['35', '41'],
+                   'Bananas': ['21', '34']},
+                  index=['2017 Sales', '2018 Sales'])
+
+file_name = "fruit.csv"
+
+df.to_csv(file_name)
